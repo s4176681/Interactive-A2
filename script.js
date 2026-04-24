cadd63c50cedf8e81c277b95f665e39d37662855
r-13472680 <s4176681@student.rmit.edu.au>
2026-04-25 00:47:26 +1000

    Finally created a playable audio widget

--- script.js
+++ script.js
@@ -1,24 +1,42 @@
-const video = document.querySelector("#custom-audio-player");
-// creating a constant variable, naming it 'audio/video'
+const audio = document.querySelector("#custom-audio-player");
+// creating a constant variable, naming it 'audio/audio'
 const playPauseBtn = document.querySelector("#play-pause-btn");
 const playPauseImg = document.querySelector("#play-pause-img");
-const progressBar = document.querySelector("#progress-bar-fill");
-video.removeAttribute("controls");
+const progressBarFill = document.querySelector("#progress-bar-fill");
+// added the progress bar const with .progress-bar, since the progress bar needed the actual container and not the fill inside of it.
+const progressBar = document.querySelector(".progress-bar");
+// removed 'default controls remover' here, since reset.css does most of that already.
 // playPauseBtn.addEventListener("click", togglePlayPause);
-// this prints the 'video' thats been defined in line 1 to be printed into the console, or the html page.
-video.addEventListener("timeupdate", updateProgressBar);
+// this prints the 'audio' thats been defined in line 1 to be printed into the console, or the html page.
+audio.addEventListener("timeupdate", updateProgressBar);
 function togglePlayPause() {
-  if (video.paused || video.ended) {
-    video.play();
+  if (audio.paused || audio.ended) {
+    audio.play();
     playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/pause--v1.png";
   } else {
-    video.pause();
+    audio.pause();
     playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/play--v1.png";
   }
 }
 function updateProgressBar() {
-  const value = (video.currentTime / video.duration) * 100;
-  progressBar.style.width = value + "%";
+  const value = (audio.currentTime / audio.duration) * 100;
+  progressBarFill.style.width = value + "%";
+  //Updates only the fill element of the container of the entire progress bar.
 }
+
 // Add other functionalities here
- 
+
+function clickProgressBar(event) {
+  // in between the brackets, it needs to accept a parameter
+  const rect = progressBar.getBoundingClientRect();
+  const clickX = event.clientX - rect.left;
+  // starts reading from the left of the progress bar, and then it divides the click on the X axis by the width.
+  const percentage = clickX / rect.width;
+  // calculating the percentage of the bar and where its been clicked
+  console.log("Clicked at this" + percentage)
+  //prints the percentage in 0.00000 form in the console.
+
+  if (audio.duration) {
+    audio.currentTime = (percentage) * audio.duration;
+  }
+}
